a42752d2664e2257c392899f6aa6e879ccea1e3a
Zhanbolat Orakbayev <fous363@gmail.com>
2022-05-28 17:09:08 +0600

    Implement solution

--- cache.go
+++ cache.go
@@ -2,22 +2,63 @@ package cache
 
 import "time"
 
+type CachePair struct {
+	key       string
+	value     string
+	expiredIn *time.Time
+}
+
 type Cache struct {
+	pair []CachePair
 }
 
 func NewCache() Cache {
 	return Cache{}
 }
 
-func (receiver) Get(key string) (string, bool) {
-
+func (cachePair *CachePair) isExpired() bool {
+	return cachePair.expiredIn != nil && time.Now().After(*cachePair.expiredIn)
 }
 
-func (receiver) Put(key, value string) {
+func (cache *Cache) removeExpired() {
+	expiredIndexes := make([]int, 0)
+	for i := 0; i < len(cache.pair); i++ {
+		if cache.pair[i].isExpired() {
+			expiredIndexes = append(expiredIndexes, i)
+		}
+	}
+	for _, expiredPairIndex := range expiredIndexes {
+		cache.pair = append(cache.pair[:expiredPairIndex], cache.pair[expiredPairIndex+1:]...)
+	}
 }
 
-func (receiver) Keys() []string {
+func (cache *Cache) Get(key string) (string, bool) {
+	cache.removeExpired()
+	for _, cachePair := range cache.pair {
+		if cachePair.key == key && !cachePair.isExpired() {
+			return cachePair.value, true
+		}
+	}
+	return "", false
 }
 
-func (receiver) PutTill(key, value string, deadline time.Time) {
+func (cache *Cache) Put(key, value string) {
+	cache.removeExpired()
+	cache.pair = append(cache.pair, CachePair{key, value, nil})
+}
+
+func (cache *Cache) Keys() []string {
+	cache.removeExpired()
+	keys := make([]string, 0, len(cache.pair))
+	for _, cachePair := range cache.pair {
+		if !cachePair.isExpired() {
+			keys = append(keys, cachePair.key)
+		}
+	}
+	return keys
+}
+
+func (cache *Cache) PutTill(key, value string, deadline time.Time) {
+	cache.removeExpired()
+	cache.pair = append(cache.pair, CachePair{key, value, &deadline})
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/golang-hw-5-2
+
+go 1.18
